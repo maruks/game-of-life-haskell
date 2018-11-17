c75320e3cbbaaf9458d308327200cb63ceb27c47
Maris Orbidans <maris.orbidans@onthemarket.com>
2018-11-17 16:05:45 +0000

    session1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,24 @@
 module Lib
-    ( Grid(..), nextGrid
+    ( Grid(..), nextGrid, numberOfNeighbours
     ) where
 
 import qualified Data.Map.Strict as Map
 
-data Grid = Grid {width :: Int,
+data Grid = Grid{width :: Int,
                   height :: Int,
-                  cells :: Map.Map (Int, Int) Bool} deriving Show
+                  cells :: Map.Map (Int, Int) Bool} deriving (Show, Eq)
 
+isAlive :: Bool -> Int -> Bool
+isAlive i x
+  | x < 2 = False
+  | x > 3 = False
+  | x == 3 && not i = True
+  | otherwise = i
 
 nextGrid:: Grid -> Grid
-nextGrid = id
+nextGrid (Grid w h cells) = Grid w h newCells
+  where newCells = Map.fromList $ map (\p -> (p,True))  [ (x,y) | x <- [0..w], y <- [0..h], isAlive (Map.member (x,y) cells) $ numberOfNeighbours (Grid w h cells) x y]
+
+numberOfNeighbours :: Grid -> Int -> Int -> Int
+numberOfNeighbours (Grid _ _ cells) xp yp =
+  let ps = [ (x,y) | x <- [-1..1], y <- [-1..1], Map.member (xp+x,yp+y) cells, not (x==0 && y==0)] in length ps
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,10 +4,19 @@ import Test.QuickCheck
 
 import Lib
 
+import qualified Data.Map.Strict as Map
+
 main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
 specs = describe "game of life" $ do
-          it "bla bla" $ do
-            2 `shouldBe` 2
+          it "next Grid returns grid" $ do
+            let g = Grid 2 2 Map.empty
+            nextGrid g `shouldBe` g
+          it "returns the number of neighbours" $ do
+            let grid = Grid 2 2 $ Map.fromList [((0,0), True), ((0,1), True), ((1,1), True)]
+              in
+              numberOfNeighbours grid 0 0 `shouldBe` 2
+          it "should cause a cell to die if it has less than 2 neighbours" $ do
+            let grid = Grid 2 2 $ Map.fromList [((0,1), True), ((1,1), True)] in cells ( nextGrid grid ) `shouldBe` Map.empty
